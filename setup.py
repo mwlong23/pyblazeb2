a148a2693ae6aff1182be2ff492a25acecbf173f
Mitch Long <meechllada@gmail.com>
2018-07-24 15:18:16 -0700

    bump version to 0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyblazeb2",
-    version="0.12",
+    version="0.13",
     author="Mitchell Long",
     author_email='meechllada@gmail.com',
     description="A compact library for interacting with Backblaze b2 buckets",
